a2852eb4919c78f2fdbd4985dfad68c91fc19c65
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-07-18 17:11:11 +0700

    fixed stuff for gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./MainPage.tsx";
 import Gallery from "./Gallery.tsx";
 import AddSchedule from "./AddSchedule.tsx";
@@ -12,53 +12,27 @@ import Status from "./Status.tsx";
 import VideoCall from "./VideoCall.tsx";
 import { FeederProvider } from "./context/FeederContext.tsx";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainPage />,
-    },
-    {
-        path: "/gallery",
-        element: <Gallery />,
-    },
-    {
-        path: "/add-schedule",
-        element: <AddSchedule />,
-    },
-    {
-        path: "/change-schedule",
-        element: <ChangeSchedule />,
-    },
-    {
-        path: "/enviroment",
-        element: <Environment />,
-    },
-    {
-        path: "/log",
-        element: <Log />,
-    },
-    {
-        path: "/pet-status",
-        element: <PetStatus />,
-    },
-    {
-        path: "/device-status",
-        element: <Status />,
-    },
-    {
-        path: "/video-call",
-        element: <VideoCall />,
-    },
-     {
-        path: "/add-schedule",
-        element: <AddSchedule />,
-    },
-]);
+const App = () => (
+    <FeederProvider>
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/add-schedule" element={<AddSchedule />} />
+                <Route path="/change-schedule" element={<ChangeSchedule />} />
+                <Route path="/environment" element={<Environment />} />
+                <Route path="/log" element={<Log />} />
+                <Route path="/pet-status" element={<PetStatus />} />
+                <Route path="/device-status" element={<Status />} />
+                <Route path="/video-call" element={<VideoCall />} />
+                <Route path="/add-schedule" element={<AddSchedule />} />
+            </Routes>
+        </Router>
+    </FeederProvider>
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <FeederProvider>
-            <RouterProvider router={router} />
-        </FeederProvider>
+        <App />
     </React.StrictMode>
 );
